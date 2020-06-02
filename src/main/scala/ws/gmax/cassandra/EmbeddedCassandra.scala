1f4506a3902c04d2ea1b890298e5a7e6df332706
= <=>
2020-06-02 21:46:43 +0300

    tesst in VSC

--- src/main/scala/ws/gmax/cassandra/EmbeddedCassandra.scala
+++ src/main/scala/ws/gmax/cassandra/EmbeddedCassandra.scala
@@ -12,6 +12,9 @@ import org.cassandraunit.utils.EmbeddedCassandraServerHelper
   */
 trait EmbeddedCassandra {
 
+  val DEFAULT_HOST = "127.0.0.1"
+  val DEFAULT_PORT = 9042
+
   val tmp = System.getProperty("java.io.tmpdir")
   val base = "embeddedCassandra"
 
@@ -22,27 +25,30 @@ trait EmbeddedCassandra {
     * Start embedded Cassandra, connect and get a session.
     * Load test data if a data set is provided.
     *
-    * @param address host default localhost (127.0.0.1)
+    * @param host host default localhost (127.0.0.1)
     * @param port    port default 9042
     * @param dataSet optional data set
     * @return session
     */
-  def startUp(address: String = "127.0.0.1", port: Int = 9042, dataSet: Option[CQLDataSet] = None): Session = {
+  def startUp(host: String = DEFAULT_HOST, port: Int = DEFAULT_PORT, dataSet: Option[CQLDataSet] = None): Session = {
     EmbeddedCassandraServerHelper.startEmbeddedCassandra(yamlFile, tmpDir)
     val cluster = Cluster.builder
-      .addContactPoint(address).withPort(port)
+      .addContactPoint(host).withPort(port)
       .withPoolingOptions(new PoolingOptions()
         .setMaxRequestsPerConnection(HostDistance.LOCAL, 4096)
         .setMaxQueueSize(1024)
         .setConnectionsPerHost(HostDistance.LOCAL, 4, 8))
       .build
     val session = cluster.connect
+    loadData(session, dataSet)
+    session
+  }
 
+  // initialize database
+  def loadData(session: Session, dataSet: Option[CQLDataSet]) =
     dataSet foreach { data =>
       new CQLDataLoader(session).load(data)
     }
-    session
-  }
 
   /**
     * Clean up
